adeedeb1c743d3e3335345c81b4230ff54a3d45d
Matias <mpierdominici@itba.edu.ar>
2018-09-10 17:57:59 -0300

    se pprobo todo hasta ahora

--- channel.m
+++ channel.m
@@ -4,13 +4,15 @@ classdef channel
         verticalScale
         probe
         offset
+        status
     end
     methods
-        function obj = channel(channelNumber,verticalScale,probe,offset)
+        function obj = channel(channelNumber,verticalScale,probe,offset,status)
             obj.channelNumber=channelNumber;
             obj.verticalScale=verticalScale;
             obj.probe=probe;
             obj.offset=offset;
+            obj.status=status;
         end
         
     end
